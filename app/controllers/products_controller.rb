593d593fd44290dbd92a5c20a57ed48276f1ffc2
nospame <mapson.evan@gmail.com>
2022-03-09 10:16:02 -0800

    display first or all products, add Faker gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
   def show_first
-    render json: {message: "show the first product"}
+    first_product = Product.first
+    render json: {
+      name: first_product.name, 
+      price: first_product.price, image_url: first_product.image_url, description: first_product.description}
   end
 
   def show_all
-    render json: {message: "show all products"}
+    all_products = Product.all
+    render json: {products: all_products}
   end
 end
