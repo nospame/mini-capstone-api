9ccc40c610a0c382d950af27bacef95163435d7f
nospame <mapson.evan@gmail.com>
2022-03-09 11:08:43 -0800

    add TTY gem to show products as table

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "bootsnap", require: false
 # Faker gem
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
+# tty-table gem
+gem "tty-table"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
---
+++ http_frontend.rb
@@ -0,0 +1,37 @@
+require 'http'
+require 'tty-table'
+
+# first_product = HTTP.get "http://localhost:3000/first-item"
+
+# first_product = first_product.parse(:json)
+
+# puts "Product name: #{first_product["name"]}"
+# puts "Description: #{first_product["description"]}"
+# puts "Price: $#{first_product["price"]}"
+# puts "Image: #{first_product["image_url"]}"
+
+
+all_products = HTTP.get "http://localhost:3000/all-items"
+all_products = all_products.parse(:json)["products"]
+# puts all_products.to_a
+
+# all_products.each do |product|
+#   puts "Product name: #{product["name"]}"
+#   puts "Description: #{product["description"]}"
+#   puts "Price: $#{product["price"]}"
+#   puts "Image: #{product["image_url"]}"
+#   puts ""
+# end
+
+product_table = TTY::Table.new(["Name", "Price", "Description"],[])
+product_array = []
+all_products.each do |product|
+  product_array = [product["name"], product["price"], product["description"]]
+  product_table << product_array
+end
+
+puts product_table.render(:ascii)
+
+
+
+
