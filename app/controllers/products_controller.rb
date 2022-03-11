1034c9b21659f30a4876cc037acc28d2d7eef105
nospame <mapson.evan@gmail.com>
2022-03-11 10:21:20 -0800

    add products index, show, and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,21 @@
 class ProductsController < ApplicationController
+  def index
+    all_products = Product.all
+    render json: all_products.as_json
+  end
 
+  def show
+    show_product = Product.find_by(id: params[:id])
+    render json: show_product.as_json
+  end
 
+  def create
+    Product.create(
+      name: params[:name],
+      price: params[:price].to_i,
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    render json: Product.last.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
