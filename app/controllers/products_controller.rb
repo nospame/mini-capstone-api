7efc08f250808fd25e9cf62df6e1fae8d6f3c03b
nospame <mapson.evan@gmail.com>
2022-03-11 11:22:19 -0800

    add product update and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,51 @@
 class ProductsController < ApplicationController
   def index
-    all_products = Product.all
-    render json: all_products.as_json
+    products = Product.all
+    render json: products.as_json
   end
 
   def show
-    show_product = Product.find_by(id: params[:id])
-    render json: show_product.as_json
+    product = Product.find_by(id: params[:id])
+    render json: product.as_json
   end
 
   def create
-    Product.create(
+    product = Product.new(
       name: params[:name],
       price: params[:price].to_i,
       description: params[:description],
       image_url: params[:image_url]
     )
+    product.save
     render json: Product.last.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+
+    product.update(
+      name: params[:name] || product.name,
+      price: params[:price] || product.price,
+      description: params[:description] || product.description,
+      image_url: params[:image_url] || product.image_url
+    )
+
+    # product.name = params[:name] || product.name
+    # product.price = params[:price] || product.price
+    # product.description = params[:description] || product.description
+    # product.image_url = params[:image_url] || product.image_url
+    # product.save
+
+    render json: product.as_json
+  end
+
+  def destroy
+    if Product.find_by(id: params[:id])
+      Product.destroy(params[:id])
+      message = "Product #{params[:id]} deleted."
+    else
+      message = "No such product."
+    end
+    render json: {message: message}
   end
 end
