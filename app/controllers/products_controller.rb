4fb648bc142250748408737ccb5c97c2d46bc124
nospame <mapson.evan@gmail.com>
2022-03-11 10:02:27 -0800

    remove all routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,4 @@
 class ProductsController < ApplicationController
-  def show_first
-    first_product = Product.first
-    render json: {
-      name: first_product.name, 
-      price: first_product.price, image_url: first_product.image_url, description: first_product.description}
-  end
 
-  def show_all
-    all_products = Product.all
-    render json: all_products.as_json
-  end
-
-  def show_last
-    last_product = Product.last
-    render json: {
-      name: last_product.name, 
-      price: last_product.price, image_url: last_product.image_url, description: last_product.description}
-  end
-
-  def by_id
-    product_id = params[:id]
-    product_info = Product.find(product_id)
-    render json: product_info.as_json
-  end
-
-  def by_name
-    product_name = params[:name]
-    product_info = Product.find_by(name: product_name)
-    render json: product_info.as_json
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/first-item" => "products#show_first"
-  get "/all-items" => "products#show_all"
-  get "/last-item" => "products#show_last"
-  get "/lookup-id" => "products#by_id"
-  get "/lookup-id/:id" => "products#by_id"
-  get "/lookup-name" => "products#by_name"
-  get "/lookup-name/:name" => "products#by_name"
+
 end
