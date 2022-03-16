922b0c5f261888730993cec86aa3fe42003f4ef0
nospame <mapson.evan@gmail.com>
2022-03-16 15:51:10 -0700

    create database dump seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "coffee mug", price: "8.0", image_url: "https://images.squarespace-cdn.com/content/v1/52e56f16e4b0d1eb5c77cfbf/1525467849214-VGM0KT7H15VYZ83TMRYQ/First+I+Drink+the+Coffee+-+Stash+Style+Ceramic+Mug.jpg?format=1500w", description: "ceramic with a round handle", on_hand: 1},
+  {name: "Polaroid", price: "40.0", image_url: "https://m.media-amazon.com/images/I/51p2Ta2LOUL._AC_SY355_.jpg", description: "An old camera to take instant pictures. Or maybe they make new ones?", on_hand: 1},
+  {name: "mouse", price: "13.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/2/22/3-Tasten-Maus_Microsoft.jpg", description: "move it around to do things on the computer", on_hand: 4},
+  {name: "Hershey's Chocolate Cup", price: "2.0", image_url: "https://i.pinimg.com/originals/00/2f/5e/002f5e8ad34dcbb53bfe4e28a6c7538f.jpg", description: "Peanut butter, chocolate cookie, milk chocolate", on_hand: 8},
+  {name: "microphone", price: "40.0", image_url: "https://m.media-amazon.com/images/I/61shDrGJbQS._AC_SL1500_.jpg", description: "takes sound from the real world and puts it in your computer", on_hand: 2},
+  {name: "fiddle-leaf fig", price: "60.0", image_url: "https://bloomscape.com/wp-content/uploads/2020/03/bloomscape_ficus-little-fiddle_charcoal-e1625250864922.jpg", description: "a finicky potted plant", on_hand: 2}
+])
