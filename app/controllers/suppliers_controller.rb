4b46dd7bddfd741cf4fdf4ad76ab5ec880d24f1b
nospame <mapson.evan@gmail.com>
2022-03-18 09:40:39 -0700

    add all actions for Suppliers

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,36 @@
 class SuppliersController < ApplicationController
+  def index
+    suppliers = Supplier.all
+    render json: suppliers.as_json
+  end
+
+  def show
+    supplier = Supplier.find_by(id: params[:id])
+    render json: supplier.as_json
+  end
+
+  def create
+    supplier = Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    supplier.save
+    render json: supplier.as_json
+  end
+
+  def update
+    supplier = Supplier.find_by(id: params[:id])
+    supplier.name = params[:name] || supplier.name
+    supplier.email = params[:email] || supplier.email
+    supplier.phone_number = params[:phone_number] || supplier.phone_number
+    supplier.save
+    render json: supplier.as_json
+  end
+
+  def destroy
+    supplier = Supplier.find_by(id: params[:id])
+    supplier.destroy
+    render json: {message: "Supplier deleted."}
+  end
 end
