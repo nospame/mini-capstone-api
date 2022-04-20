1d4a80a71f90f87b723773a650673fc2991251ba
nospame <mapson.evan@gmail.com>
2022-04-20 11:39:48 -0700

    add netlify frontend url to CORS, use requests.http to test production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "mini-capstone.peterxjang.com"
+    origins "localhost:8080", "mini-capstone.peterxjang.com", "jolly-granita-65ae9f.netlify.app"
 
     resource "*",
       headers: :any,
